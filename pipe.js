70570295824ca7c388b6907e077f6c1b53267936
Dharmendra <dharmendra17893@gmail.com>
2017-04-12 23:45:13 +0530

    Theses topics are covered

---
+++ pipe.js
@@ -0,0 +1,10 @@
+var request = require('request');
+var fs = require('fs');
+var zlib = require('zlib');
+
+// request('http://www.pluralsight.com/').pipe(fs.createWriteStream('pluralsight.html'));
+request('http://www.pluralsight.com/').pipe(zlib.createGzip()).pipe(fs.createWriteStream('pluralsight.html.gz'));
+// var s = request('http://www.pluralsight.com/');
+
+// s.pipe(process.stdout);
+// request('http://www.pluralsight.com/').pipe(process.stdout);
